bd3cd4dde666fae7ca31fe5e82af0c755d052f6a
rubinigor-star <rubin.igor@gmail.com>
2026-07-04 08:24:04 +0300

    Fix JSX build config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './'
+  base: './',
+  esbuild: {
+    loader: 'jsx',
+    include: /.*\.js$/
+  }
 });
