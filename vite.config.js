34850a5292ccb6be002db2545060378cae85236e
rubinigor-star <rubin.igor@gmail.com>
2026-07-04 08:40:13 +0300

    Simplify Vite config for vanilla build

--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,5 @@
-import { defineConfig, transformWithEsbuild } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: './',
-  plugins: [
-    {
-      name: 'treat-js-files-as-jsx',
-      enforce: 'pre',
-      async transform(code, id) {
-        if (!id.endsWith('/main.js')) return null;
-        return transformWithEsbuild(code, id, {
-          loader: 'jsx',
-          jsx: 'automatic'
-        });
-      }
-    },
-    react()
-  ],
-  optimizeDeps: {
-    esbuildOptions: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  }
+  base: './'
 });
