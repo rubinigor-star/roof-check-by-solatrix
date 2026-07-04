ad52cf74167827be18af96a06346c248bcfd5594
rubinigor-star <rubin.igor@gmail.com>
2026-07-04 08:28:37 +0300

    Fix Vite JSX transform

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,27 @@
-import { defineConfig } from 'vite';
+import { defineConfig, transformWithEsbuild } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [react()],
   base: './',
-  esbuild: {
-    loader: 'jsx',
-    include: /.*\.js$/
+  plugins: [
+    {
+      name: 'treat-js-files-as-jsx',
+      enforce: 'pre',
+      async transform(code, id) {
+        if (!id.endsWith('/main.js')) return null;
+        return transformWithEsbuild(code, id, {
+          loader: 'jsx',
+          jsx: 'automatic'
+        });
+      }
+    },
+    react()
+  ],
+  optimizeDeps: {
+    esbuildOptions: {
+      loader: {
+        '.js': 'jsx'
+      }
+    }
   }
 });
